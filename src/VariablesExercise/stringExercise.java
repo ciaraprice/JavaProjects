44d2362fa923ed0315e4e49388b23eababde0f4a
ciaraprice <ciarasprice20@gmail.com>
2023-09-08 12:28:12 +0100

    assigning string and ints

---
+++ src/VariablesExercise/stringExercise.java
@@ -0,0 +1,29 @@
+package VariablesExercise;
+public class stringExercise {
+
+    public static void main(String[]args){
+
+        String firstName = "Ciara";
+        String lastName = "Price";
+
+        String name = (firstName + " " + lastName);
+
+
+        System.out.println(name);
+
+        int num1 = 12;
+        double num2 = 7;
+
+        double result = num1 / num2;
+
+        int num3 = 8;
+
+        System.out.println(num1 + num2);
+        System.out.println(result);
+        System.out.println(num3++);
+        System.out.println(num3);
+        System.out.println(++num3);
+    }
+}
+
+
